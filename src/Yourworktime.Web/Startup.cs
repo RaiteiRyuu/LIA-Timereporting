4ad6b17b59597d52fe589afe49e628036b9da1bf
Emir Jasarevic <emir2000@hotmail.se>
2020-09-28 15:07:43 +0200

    Removed default pages

--- src/Yourworktime.Web/Startup.cs
+++ src/Yourworktime.Web/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Yourworktime.Web.Data;
+
 
 namespace Yourworktime.Web
 {
@@ -28,7 +28,6 @@ namespace Yourworktime.Web
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
