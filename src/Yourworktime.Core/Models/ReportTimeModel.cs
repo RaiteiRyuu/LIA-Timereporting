6c624cd673339902b3fd8098a85f35ec3280ba6f
Emir Jasarevic <emir2000@hotmail.se>
2020-10-10 17:08:14 +0200

    Created ReportTimeModel

---
+++ src/Yourworktime.Core/Models/ReportTimeModel.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Yourworktime.Core.Models
+{
+    class ReportTimeModel
+    {
+        public string Task { get; set; }
+        public string Project { get; set; }
+        public string Tag { get; set; }
+        public string Time { get; set; }
+        public string Duration { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
