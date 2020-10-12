9d944cd867a05e207c2c83238a403880dbaa7eed
Emir Jasarevic <emir2000@hotmail.se>
2020-10-12 15:25:25 +0200

    created reporttime service

---
+++ src/Yourworktime.Core/Models/ReportTimeModel.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MongoServer.Core.Models
+{
+    public class ReportTimeModel
+    {
+        [BsonId]
+        public Guid Id { get; set; }
+        public Guid Owner { get; set; }
+        public string Task { get; set; }
+        public string Project { get; set; }
+        public string Tag { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+        public DateTime CreationDate { get; set; }
+    }
+}
