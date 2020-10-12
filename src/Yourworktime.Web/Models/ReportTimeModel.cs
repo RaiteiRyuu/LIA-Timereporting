b80210741e4d98ab07b45360775f8c3b30b01d09
Emir Jasarevic <emir2000@hotmail.se>
2020-10-12 10:42:59 +0200

    Model updated

--- src/Yourworktime.Web/Models/ReportTimeModel.cs
+++ src/Yourworktime.Web/Models/ReportTimeModel.cs
@@ -12,7 +12,7 @@ namespace Yourworktime.Core.Models
         public string Tag { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime EndTime { get; set; }
-        public DateTime Duration { get; set; }
+        public TimeSpan Duration { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }
