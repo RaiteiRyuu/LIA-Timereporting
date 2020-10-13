edd08bc78f3a4ce8c4a1545d657732bdea5252fa
Ryuu <evil__dark__angel@hotmail.com>
2020-10-13 10:58:51 +0200

    workspace image

--- src/Yourworktime.Core/Models/WorkspaceModel.cs
+++ src/Yourworktime.Core/Models/WorkspaceModel.cs
@@ -10,7 +10,7 @@ namespace Yourworktime.Core.Models
         [BsonId]
         public Guid Id { get; set; }
         public string  Name { get; set; }
-        public string ProfileImagePath { get; set; }
+        public string WorkspaceImagePath { get; set; }
         public Guid Owner { get; set; }
         public Guid[] Members { get; set; }
         public DateTime RegisteredTime { get; set; }
